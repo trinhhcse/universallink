a604398a8ce9e096a4944e3700f491c9c75b096b
trinh.ho <trinh.ho@tpssoft.com>
2019-06-03 14:55:11 +0700

    init

---
+++ src/main/java/com/trinhhc/demo/UniversallinkApplication.java
@@ -0,0 +1,13 @@
+package com.trinhhc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversallinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversallinkApplication.class, args);
+    }
+
+}
